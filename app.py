404d778584696cbe540175bf2932df173164754d
evan-wes <evan.j.sci@gmail.com>
2021-04-27 17:53:47 -0400

    initial commit

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 #from flask_login import LoginManager
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///database.db'
+if environ.get('DATABASE_URL') is None:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL').replace('postgres', 'postgresql')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 
