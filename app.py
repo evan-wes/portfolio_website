7315dd64c3c43d6bb393e4046eaa78f939d26f4f
evan-wes <evan.j.sci@gmail.com>
2021-04-27 17:35:23 -0400

    initial commit

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ db = SQLAlchemy(app)
 
 import routes
 
-'''
+
 from models import Project
 
 # Create database and add projects
@@ -22,14 +22,10 @@ db.drop_all()
 db.create_all()
 
 # Create Project instances
-naive_bayes_sklearn_project = Project(id_name='naive_bayes_sklearn', 
-                                        name='Naive-Bayes Classifcation with sklearn', 
-                                        html_filename='naive_bayes_sklearn.html',
-                                        heroku_link='https://naive-bayes-sklearn.herokuapp.com/')
+naive_bayes_sklearn_project = Project(id_name='naive_bayes_sklearn',name='Naive-Bayes Classifcation with sklearn',html_filename='naive_bayes_sklearn.html',heroku_link='https://naive-bayes-sklearn.herokuapp.com/')
 
 # Add Project instances to database
 db.session.add(naive_bayes_sklearn_project)
 
 # Commit changes to database
 db.session.commit()
-'''
